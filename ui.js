607ab746971d81fad0036bf4416ed69ddbf4931a
leo <familiehn.leo@gmail.com>
2020-04-26 20:53:01 +0200

    Improved menu structure

--- ui.js
+++ ui.js
@@ -1,20 +1,22 @@
-function toggle_additional_info_container(clicked_button) {
-    additional_info_container = document.getElementById("additional_info_container");
-
-    if (additional_info_container.style.display == "grid") {
-        additional_info_container.style.display = "none";
-        clicked_button.innerText = "+";
-    }
-
-    else {
-        additional_info_container.style.display = "grid";
-        clicked_button.innerText = "-";
-    }
-}
-
-
-
 window.addEventListener("load", function() {
+    var submenu_toggle_buttons = document.querySelectorAll(".submenu_header > button");
+
+    for (let button of submenu_toggle_buttons) {
+        button.addEventListener("click", function(e) {
+            e.target.parentElement.parentElement.getElementsByClassName("submenu_content")[0].classList.toggle("closed");
+
+            if (e.target.innerText == "+") {
+                e.target.innerText = "-";
+            }
+
+            else {
+                e.target.innerText = "+";
+            }
+        });
+    }
+
+
+
     var car_select = document.getElementById("car_select");
 
     for (let car_index in car_data) {
